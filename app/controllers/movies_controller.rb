aa441386d9d781a6c41bb3f2916fdacd4e5a3140
juan <jacobo12.montes@gmail.com>
2021-01-29 00:59:33 -0500

    Add a basic search form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    @movies = Movie.search(params[:search])
   end
 
   # GET /movies/1 or /movies/1.json
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,12 @@
 class Movie < ApplicationRecord
+    self.inheritance_column = "not_sti"
     belongs_to :user
+
+    def self.search(search)
+        if search
+          where(["title LIKE ?","%#{search}%"])
+        else
+          all
+        end  
+      end   
 end
